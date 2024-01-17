a8d609784946155a7720b6097ed9d00fe73e9765
venom bolteop <145913044+venombolteop@users.noreply.github.com>
2024-01-17 16:53:12 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,32 @@
+
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv("config.env")
+
+
+API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", ""))
+BLACKLIST_CHAT = getenv("BLACKLIST_CHAT", None)
+if not BLACKLIST_CHAT:
+    BLACKLIST_CHAT = [-1001649838443]
+BLACKLIST_GCAST = {int(x) for x in getenv("BLACKLIST_GCAST", "").split()}
+BOTLOG_CHATID = int(getenv("BOTLOG_CHATID", "") or 0)
+BOT_VER = "0.1.0@main"
+BRANCH = getenv("BRANCH", "main")
+CHANNEL = getenv("CHANNEL", "VenomOwners")
+CMD_HANDLER = getenv("CMD_HANDLER", ".")
+DB_URL = getenv("DATABASE_URL", "")
+GIT_TOKEN = getenv("GIT_TOKEN", "")
+GROUP = getenv("GROUP", "Venom_Chatz")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
+PMPERMIT_PIC = getenv("PMPERMIT_PIC", "https://te.legra.ph/file/0b373de1c657129297c39.jpg")
+REPO_URL = getenv("REPO_URL", "https://github.com/venombolteop/VenomX")
+STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION2 = getenv("STRING_SESSION2", "")
+STRING_SESSION3 = getenv("STRING_SESSION3", "")
+STRING_SESSION4 = getenv("STRING_SESSION4", "")
+STRING_SESSION5 = getenv("STRING_SESSION5", "")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))

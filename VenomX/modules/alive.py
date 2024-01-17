5946ea94ec205098c2dbca870c2f6506e7f12e6d
venom bolteop <145913044+venombolteop@users.noreply.github.com>
2024-01-17 21:04:49 +0530

    Add files via upload

--- VenomX/modules/alive.py
+++ VenomX/modules/alive.py
@@ -2,16 +2,13 @@ import asyncio
 import os
 import time
 from platform import python_version
-
-from pyrogram import Client
 from pyrogram import __version__ as versipyro
-from pyrogram import filters
+
+from pyrogram import Client, filters
 from pyrogram.types import Message
 from telegraph import exceptions, upload_file
 
-from config import BOT_VER, CHANNEL
-from config import CMD_HANDLER as cmd
-from config import GROUP
+from config import BOT_VER, CMD_HANDLER as cmd, CHANNEL, GROUP
 from VenomX import CMD_HELP, StartTime
 from VenomX.helpers.basic import edit_or_reply
 from VenomX.helpers.PyroHelpers import ReplyCheck
@@ -23,149 +20,143 @@ from VenomX.utils.misc import restart
 from .help import add_command_help
 
 modules = CMD_HELP
-alive_logo = (
-     gvarstatus("ALIVE_LOGO") or "https://te.legra.ph/file/0b373de1c657129297c39.jpg"
-)
+alive_logo = gvarstatus("ALIVE_LOGO") or "https://te.legra.ph/file/0b373de1c657129297c39.jpg"
 emoji = gvarstatus("ALIVE_EMOJI") or "⚡️"
 alive_text = gvarstatus("ALIVE_TEKS_CUSTOM") or "Hey, I am alive."
 
-
 @Client.on_message(filters.command(["alive", "awake"], cmd) & filters.me)
 async def alive(client: Client, message: Message):
-     xx = await edit_or_reply(message, "⚡️")
-     await asyncio.sleep(2)
-     send = client.send_video if alive_logo.endswith(".mp4") else client.send_photo
-     uptime = await get_readable_time((time.time() - StartTime))
-     man = (
-         f"**[VenomX-Userbot](https://github.com/venombolteop/VenomX) is Up and Running.**\n\n"
-         f"<b>{alive_text}</b>\n\n"
-         f"{emoji} <b>Master :</b> {client.me.mention} \n"
-         f"{emoji} <b>Modules :</b> <code>{len(modules)} Modules</code> \n"
-         f"{emoji} <b>Bot Version :</b> <code>{BOT_VER}</code> \n"
-         f"{emoji} <b>Python Version :</b> <code>{python_version()}</code> \n"
-         f"{emoji} <b>Pyrogram Version :</b> <code>{pyro version}</code> \n"
-         f"{emoji} <b>Bot Uptime :</b> <code>{uptime}</code> \n\n"
-         f" **[𝗦𝘂𝗽𝗽𝗼𝗿𝘁](https://t.me/{GROUP})** | **[𝗖𝗵𝗮𝗻𝗻𝗲𝗹](https://t.me/{CHANNEL})** | **[𝗢𝘄𝗻𝗲𝗿]( tg://user?id={client.me.id})**"
-     )
-     try:
-         await asyncio.gather(
-             xx.delete(),
-             send(
-                 message.chat.id,
-                 alive_logo,
-                 caption=man,
-                 reply_to_message_id=ReplyCheck(message),
-             ),
-         )
-     except BaseException:
-         await xx.edit(man, disable_web_page_preview=True)
-
+    xx = await edit_or_reply(message, "⚡️")
+    await asyncio.sleep(2)
+    send = client.send_video if alive_logo.endswith(".mp4") else client.send_photo
+    uptime = await get_readable_time((time.time() - StartTime))
+    man = (
+        f"**[VenomX-Userbot](https://github.com/venombolteop/VenomX) is Up and Running.**\n\n"
+        f"<b>{alive_text}</b>\n\n"
+        f"{emoji} <b>Master :</b> {client.me.mention} \n"
+        f"{emoji} <b>Modules :</b> <code>{len(modules)} Modules</code> \n"
+        f"{emoji} <b>Bot Version :</b> <code>{BOT_VER}</code> \n"
+        f"{emoji} <b>Python Version :</b> <code>{python_version()}</code> \n"
+        f"{emoji} <b>Pyrogram Version :</b> <code>{versipyro}</code> \n"
+        f"{emoji} <b>Bot Uptime :</b> <code>{uptime}</code> \n\n"
+        f"**[𝗦𝘂𝗽𝗽𝗼𝗿𝘁](https://t.me/{GROUP})** | **[𝗖𝗵𝗮𝗻𝗻𝗲𝗹](https://t.me/{CHANNEL})** | **[𝗢𝘄𝗻𝗲𝗿]( tg://user?id={client.me.id})**"
+    )
+    try:
+        await asyncio.gather(
+            xx.delete(),
+            send(
+                message.chat.id,
+                alive_logo,
+                caption=man,
+                reply_to_message_id=ReplyCheck(message),
+            ),
+        )
+    except BaseException:
+        await xx.edit(man, disable_web_page_preview=True)
 
 @Client.on_message(filters.command("setalivelogo", cmd) & filters.me)
 async def setalivelogo(client: Client, message: Message):
-     try:
-         import VenomX.helpers.SQL.globals as sql
-     except AttributeError:
-         await message.edit("**Running on Non-SQL mode!**")
-         return
-     Man = await edit_or_reply(message, "`Processing...`")
-     link = (
-         message.text.split(None, 1)[1]
-         if len(
-             message.command,
-         )
-         != 1
-         else None
-     )
-     if message.reply_to_message.media:
-         if message.reply_to_message.sticker:
-             m_d = await convert_to_image(message, client)
-         else:
-             m_d = await message.reply_to_message.download()
-         try:
-             media_url = upload_file(m_d)
-         except exceptions.TelegraphException as exc:
-             await Man.edit(f"**ERROR:** `{exc}`")
-             os.remove(m_d)
-             return
-         link = f"https://telegra.ph/{media_url[0]}"
-         os.remove(m_d)
-     sql.addgvar("ALIVE_LOGO", link)
-     await Man.edit(
-         f"**Successfully Customized ALIVE LOGO into {link}**",
-         disable_web_page_preview=True,
-     )
-     restart()
-
+    try:
+        import VenomX.helpers.SQL.globals as sql
+    except AttributeError:
+        await message.edit("**Running on Non-SQL mode!**")
+        return
+    Man = await edit_or_reply(message, "`Processing...`")
+    link = (
+        message.text.split(None, 1)[1]
+        if len(
+            message.command,
+        )
+        != 1
+        else None
+    )
+    if message.reply_to_message.media:
+        if message.reply_to_message.sticker:
+            m_d = await convert_to_image(message, client)
+        else:
+            m_d = await message.reply_to_message.download()
+        try:
+            media_url = upload_file(m_d)
+        except exceptions.TelegraphException as exc:
+            await Man.edit(f"**ERROR:** `{exc}`")
+            os.remove(m_d)
+            return
+        link = f"https://telegra.ph/{media_url[0]}"
+        os.remove(m_d)
+    sql.addgvar("ALIVE_LOGO", link)
+    await Man.edit(
+        f"**Successfully Customized ALIVE LOGO into {link}**",
+        disable_web_page_preview=True,
+    )
+    restart()
 
 @Client.on_message(filters.command("setalivetext", cmd) & filters.me)
 async def setalivetext(client: Client, message: Message):
-     try:
-         import VenomX.helpers.SQL.globals as sql
-     except AttributeError:
-         await message.edit("**Running on Non-SQL mode!**")
-         return
-     text = (
-         message.text.split(None, 1)[1]
-         if len(
-             message.command,
-         )
-         != 1
-         else None
-     )
-     if message.reply_to_message:
-         text = message.reply_to_message.text or message.reply_to_message.caption
-     Man = await edit_or_reply(message, "`Processing...`")
-     if not text:
-         return await edit_or_reply(
-             message, "**Give a Text or Reply to text**"
-         )
-     sql.addgvar("ALIVE_TEXT_CUSTOM", text)
-     await Man.edit(f"**Successfully Customized ALIVE TEXT to** `{text}`")
-     restart()
-
+    try:
+        import VenomX.helpers.SQL.globals as sql
+    except AttributeError:
+        await message.edit("**Running on Non-SQL mode!**")
+        return
+    text = (
+        message.text.split(None, 1)[1]
+        if len(
+            message.command,
+        )
+        != 1
+        else None
+    )
+    if message.reply_to_message:
+        text = message.reply_to_message.text or message.reply_to_message.caption
+    Man = await edit_or_reply(message, "`Processing...`")
+    if not text:
+        return await edit_or_reply(
+            message, "**Give a Text or Reply to text**"
+        )
+    sql.addgvar("ALIVE_TEXT_CUSTOM", text)
+    await Man.edit(f"**Successfully Customized ALIVE TEXT to** `{text}`")
+    restart()
 
 @Client.on_message(filters.command("setemoji", cmd) & filters.me)
 async def setemoji(client: Client, message: Message):
-     try:
-         import VenomX.helpers.SQL.globals as sql
-     except AttributeError:
-         await message.edit("**Running on Non-SQL mode!**")
-         return
-     emoji = (
-         message.text.split(None, 1)[1]
-         if len(
-             message.command,
-         )
-         != 1
-         else None
-     )
-     Man = await edit_or_reply(message, "`Processing...`")
-     if not emoji:
-         return await edit_or_reply(message, "**Give an Emoji**")
-     sql.addgvar("ALIVE_EMOJI", emoji)
-     await Man.edit(f"**Successfully Customized ALIVE EMOJI To** {emoji}")
-     restart()
-
+    try:
+        import VenomX.helpers.SQL.globals as sql
+    except AttributeError:
+        await message.edit("**Running on Non-SQL mode!**")
+        return
+    emoji = (
+        message.text.split(None, 1)[1]
+        if len(
+            message.command,
+        )
+        != 1
+        else None
+    )
+    Man = await edit_or_reply(message, "`Processing...`")
+    if not emoji:
+        return await edit_or_reply(message, "**Give an Emoji**")
+    sql.addgvar("ALIVE_EMOJI", emoji)
+    await Man.edit(f"**Successfully Customized ALIVE EMOJI To** {emoji}")
+    restart()
 
 add_command_help(
-     "alive",
-     [
-         [
-             "alive",
-             "To check whether your userbot is working or not",
-         ],
-         [
-             "setalivelogo <telegraph link or reply to photo/video/gif>",
-             "To customize your alive userbot logo",
-         ],
-         [
-             "setalivetext <text>",
-             "To customize your userbot's alive text",
-         ],
-         [
-             "setemoji <emoji>",
-             "To customize your userbot's alive emoji",
-         ],
-     ],
+    "alive",
+    [
+        [
+            "alive",
+            "To check whether your userbot is working or not",
+        ],
+        [
+            "setalivelogo <telegraph link or reply to photo/video/gif>",
+            "To customize your alive userbot logo",
+        ],
+        [
+            "setalivetext <text>",
+            "To customize your userbot's alive text",
+        ],
+        [
+            "setemoji <emoji>",
+            "To customize your userbot's alive emoji",
+        ],
+    ],
 )
+
--- VenomX/modules/telegraph.py
+++ VenomX/modules/telegraph.py
@@ -1,15 +1,12 @@
-
-
-
+import os
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from telegraph import Telegraph, exceptions, upload_file
 
 from config import CMD_HANDLER as cmd
 from VenomX.helpers.basic import edit_or_reply, get_text
-from VenomX.helpers.tools import *
-
-from .help import *
+from VenomX.helpers.tools import convert_to_image
+from VenomX.modules.help import add_command_help
 
 telegraph = Telegraph()
 r = telegraph.create_account(short_name="VenomX-Userbot")
@@ -20,10 +17,9 @@ auth_url = r["auth_url"]
 async def uptotelegraph(client: Client, message: Message):
     Man = await edit_or_reply(message, "`Processing . . .`")
     if not message.reply_to_message:
-        await Man.edit(
-            "**Please reply to the message, to get a link from Telegraph.**"
-        )
+        await Man.edit("**Please reply to the message to get a link from Telegraph.**")
         return
+
     if message.reply_to_message.media:
         if message.reply_to_message.sticker:
             m_d = await convert_to_image(message, client)
@@ -35,11 +31,10 @@ async def uptotelegraph(client: Client, message: Message):
             await Man.edit(f"**ERROR:** `{exc}`")
             os.remove(m_d)
             return
-        U_done = (
-            f"**Successfully uploaded to** [Telegraph](https://telegra.ph/{media_url[0]})"
-        )
+        U_done = f"**Successfully uploaded to** [Telegraph](https://telegra.ph/{media_url[0]})"
         await Man.edit(U_done)
         os.remove(m_d)
+
     elif message.reply_to_message.text:
         page_title = get_text(message) if get_text(message) else client.me.first_name
         page_text = message.reply_to_message.text
@@ -52,6 +47,6 @@ async def uptotelegraph(client: Client, message: Message):
         wow_graph = f"**Successfully uploaded to** [Telegraph](https://telegra.ph/{response['path']})"
         await Man.edit(wow_graph)
 
+# I noticed a small error in your code: add_command_help should be add_command_help() for proper usage.
+add_command_help("telegraph", [["telegraph or {cmd}tg", "Reply to Text Messages or Media to upload them to telegraph."]])
 
-add command help( "telegraph", [ [ f"telegraph or {cmd}tg", "Reply to Text Messages or Media to upload them to telegraph.", ], ],
-)
